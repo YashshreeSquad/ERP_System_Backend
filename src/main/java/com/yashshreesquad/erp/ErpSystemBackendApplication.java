a2f579be717909f9274cf2c2a00cf0039b224be5
PruthvirajHawale <hawalepruthvi12@gmail.com>
2023-11-05 20:57:39 +0530

    Pruthvi: test commit

--- src/main/java/com/yashshreesquad/erp/ErpSystemBackendApplication.java
+++ src/main/java/com/yashshreesquad/erp/ErpSystemBackendApplication.java
@@ -8,6 +8,7 @@ public class ErpSystemBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErpSystemBackendApplication.class, args);
+		System.out.println("Hello from Pruthvi..!");
 	}
 
 }
