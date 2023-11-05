8e301b700c5bf9e832e98edf811313a7398dc4b3
Shubham Patil <shubham.patil@netcracker.com>
2023-11-05 20:27:53 +0530

    Initial commit

---
+++ src/test/java/com/yashshreesquad/erp/ErpSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashshreesquad.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
