bd78095621230f5867e0da89f915626b2d954cfb
Shubham Patil <shubham.patil@netcracker.com>
2023-11-08 11:02:22 +0530

    GetEmployeeWithID and SaveEmployee implementation

--- src/main/java/com/yashshreesquad/erp/controller/EmployeeController.java
+++ src/main/java/com/yashshreesquad/erp/controller/EmployeeController.java
@@ -1,11 +1,10 @@
 package com.yashshreesquad.erp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.yashshreesquad.erp.model.Employee;
 import com.yashshreesquad.erp.repository.EmployeeRepository;
@@ -20,6 +19,16 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+
+	@GetMapping("/employees/{empId}")
+	public Optional<Employee> getEmployeeWithID(@PathVariable Long empId){
+		return employeeRepository.findById(empId);
+	}
+
+	@PostMapping("/employees")
+	public Optional<Employee> saveEmployeeDetails(@RequestBody Employee employee){
+		return Optional.of(employeeRepository.save(employee));
 	}
 
 }
--- src/main/java/com/yashshreesquad/erp/repository/EmployeeRepository.java
+++ src/main/java/com/yashshreesquad/erp/repository/EmployeeRepository.java
@@ -1,15 +1,23 @@
 package com.yashshreesquad.erp.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yashshreesquad.erp.model.Employee;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
+	@Override
 	List<Employee> findAll();
 
+	@Override
+	Optional<Employee> findById(Long id);
+
+	@Override
+	<S extends Employee> S save(S entity);
 }
